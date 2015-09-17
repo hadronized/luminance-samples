c102d5b5c83c84fe09401a1cde58bfcad8e51165
Dimitri 'phaazon' Sabadie <dimitri.sabadie@gmail.com>
2015-09-17 12:33:12 +0200

    Fixed Texture.

--- src/Texture.hs
+++ src/Texture.hs
@@ -1,16 +1,15 @@
 import Codec.Picture
 import Codec.Picture.Types
-import Control.Concurrent
+import Common
 import Control.Monad
 import Control.Monad.Except ( MonadError(..) )
 import Control.Monad.IO.Class
-import Control.Monad.Trans.Except ( runExceptT )
 import Control.Monad.Trans.Resource
-import Data.Foldable ( for_ )
 import Data.Vector ( Vector, convert )
 import qualified Data.Vector.Storable as S ( Vector )
 import Foreign.Storable ( Storable )
 import Graphics.Luminance.Batch
+import Graphics.Luminance.Cmd
 import Graphics.Luminance.Framebuffer
 import Graphics.Luminance.Geometry
 import Graphics.Luminance.RenderCmd
@@ -20,44 +19,10 @@ import Graphics.Luminance.Shader.Stage
 import Graphics.Luminance.Texture
 import Graphics.Luminance.Vertex
 import Graphics.UI.GLFW
-import Prelude hiding ( init )
 import System.Environment ( getArgs )
 
-data AppError = AppError String deriving (Eq,Show)
-
-instance HasFramebufferError AppError where
-  fromFramebufferError e = AppError (show e)
-
-instance HasStageError AppError where
-  fromStageError e = AppError (show e)
-
-instance HasProgramError AppError where
-  fromProgramError e = AppError (show e)
-
-windowW,windowH :: (Num a) => a
-windowW = 800
-windowH = 600
-
-windowTitle :: String
-windowTitle = "Texture"
-
 main :: IO ()
-main = do
-  _ <- init
-  windowHint (WindowHint'Resizable False)
-  windowHint (WindowHint'ContextVersionMajor 4)
-  windowHint (WindowHint'ContextVersionMinor 5)
-  windowHint (WindowHint'OpenGLForwardCompat False)
-  windowHint (WindowHint'OpenGLProfile OpenGLProfile'Core)
-  window <- createWindow windowW windowH windowTitle Nothing Nothing
-  makeContextCurrent window
-  for_ window $ \window' -> do
-    (runResourceT . runExceptT . app) window' >>= either print (const $ pure ())
-    destroyWindow window'
-  terminate
-
-app :: (MonadError AppError m,MonadIO m,MonadResource m) => Window -> m ()
-app window = do
+main = startup $ \window -> do
   args <- liftIO getArgs
   when (null args) . throwError $ AppError "expecting a texture path as argument!"
   tex <- loadTexture (head args)
@@ -67,12 +32,9 @@ app window = do
   (program,texU) <- createProgram [vs,fs] $ \uni -> do
     uni $ Left "srcTex"
   untilM (liftIO $ windowShouldClose window) $ do
-    treatFBBatch $ framebufferBatch defaultFramebuffer
+    void . runCmd . draw $ framebufferBatch defaultFramebuffer
       [anySPBatch $ SPBatch program texU tex [renderCmd Nothing True mempty () quad]]
-    liftIO $ do
-      pollEvents
-      swapBuffers window
-      threadDelay 50000
+    endFrame window
 
 vertices :: [V 2 Float]
 vertices =
@@ -109,18 +71,11 @@ fsSource = unlines
 
   , "void main() {"
   , "  vec2 uv = gl_FragCoord.xy;"
-  , "  uv.y = " ++ show windowH ++ " - uv.y;"
-  , "  vec3 sampled = vec3(texture(srcTex, uv / vec2(" ++ show windowW ++ "," ++ show windowH ++ "), 0).rgb) / 255;"
+  , "  uv.y = " ++ show (windowH :: Int) ++ " - uv.y;"
+  , "  vec3 sampled = vec3(texture(srcTex, uv / vec2(" ++ show (windowW :: Int) ++ "," ++ show (windowH :: Int) ++ "), 0).rgb) / 255;"
   , "  frag = vec4(sampled, 1.);"
   , "}"
   ]
-
-untilM :: (Monad m) => m Bool -> m b -> m ()
-untilM predicate a = go
-  where
-    go = do
-      p <- predicate
-      if p then pure () else a >> go
 
 loadTexture :: (MonadError AppError m,MonadIO m,MonadResource m) => String -> m (Texture2D RGBA8UI)
 loadTexture path = liftIO (readImage path) >>= either (throwError . AppError) treatDynamicImage
