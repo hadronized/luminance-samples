ed4c230f8bbb613a6a6c42a356751433c59874d0
Dimitri 'phaazon' Sabadie <dimitri.sabadie@gmail.com>
2015-09-26 11:46:47 +0200

    Used shaderProgramBatch_ and stdRenderCmd_ in Blending.

--- src/Blending.hs
+++ src/Blending.hs
@@ -24,7 +24,7 @@ main = startup $ \window -> do
     pure $ divided colorU offsetU
   untilM (liftIO $ windowShouldClose window) $ do
     void . runCmd . draw $ framebufferBatch defaultFramebuffer
-      [anySPBatch . shaderProgramBatch program mempty () $
+      [anySPBatch . shaderProgramBatch_ program $
         [
           renderCmd blending False colorOffsetU (color0,offset0) triangle 
         , renderCmd blending False colorOffsetU (color1,offset1) triangle 
